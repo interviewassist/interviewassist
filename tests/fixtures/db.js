1340b4adfe57e52521da5f2d58ea887f6795fa55
bbq9234 <bbq9234@gmail.com>
2019-06-23 23:30:17 +0900

    Add user testcase

---
+++ tests/fixtures/db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = require("../../src/models/user");
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+  _id: userOneId,
+  name: "bbq923",
+  email: "bbq923@example.com",
+  password: "56what!!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }
+  ]
+};
+
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: "leeky",
+  email: "leeky@example.com",
+  password: "myhouse099@@",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }
+  ]
+};
+
+const setupDatabase = async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+  await new User(userTwo).save();
+};
+
+module.exports = {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  setupDatabase
+};
