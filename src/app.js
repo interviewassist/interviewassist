440745cefd9c4c6ab191a1aa06ba6dbbba1e54af
bbq9234 <bbq9234@gmail.com>
2019-06-23 23:36:58 +0900

    Add user route & user create api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+require("./db/mongoose"); // requiring for mongodb connection
+const userRouter = require("./routes/user");
 
 const app = express();
+
+app.use(express.json());
+app.use(userRouter);
 
 module.exports = app;
