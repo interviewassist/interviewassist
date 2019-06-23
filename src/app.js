325c509dd0683a7ab795088562cf966318c034b4
bbq9234 <bbq9234@gmail.com>
2019-06-24 00:18:30 +0900

    Add demo welcome page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
+const path = require("path");
 const express = require("express");
 require("./db/mongoose"); // requiring for mongodb connection
 const userRouter = require("./routes/user");
+const pageRouter = require("./routes/page");
 
 const app = express();
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
 app.use(express.json());
 app.use(userRouter);
+app.use(pageRouter);
 
 module.exports = app;
