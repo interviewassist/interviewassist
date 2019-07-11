fcafbe4ba06104bc4558d690f671c9fab037ce73
bbq923 <bbq9234@naver.com>
2019-07-11 10:29:24 +0900

    Integrate Vue with Express

--- src/app.js
+++ src/app.js
@@ -6,14 +6,17 @@ const pageRouter = require("./routes/page");
 
 const app = express();
 
+app.use(require('connect-history-api-fallback')());
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
+
 app.use(express.json());
-app.use(userRouter);
 app.use(pageRouter);
+app.use(userRouter);
 
 module.exports = app;
--- src/routes/page.js
+++ src/routes/page.js
@@ -1,4 +1,9 @@
-const express = require("express");
-const router = new express.Router();
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../../public', 'index.html'))
+});
 
 module.exports = router;
